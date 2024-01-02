3c8e407a2e82085c7f2472617f7823a53cc95e40
ANURAG TIWARI <86867000+Danuragtiwari@users.noreply.github.com>
2024-01-02 19:00:21 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,75 @@
 document.addEventListener("DOMContentLoaded", function() {
   new SweetScroll({});
   particlesJS("particles-js", {
-    // ... (other configurations)
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "polygon",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nb_sides: 5
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100
+        }
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 19.18081918081918,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 4,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      },
+      nb: 80
+    },
     interactivity: {
       detect_on: "canvas",
       events: {
